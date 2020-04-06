65595356eff22c8b88439ee8c5cf934c57fb397d
novitaguok <novitaguok@gmail.com>
2020-04-07 00:55:10 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -54,42 +54,28 @@ module.exports = async function App(context) {
   if (context.event.isText) {
     await context.sendText(`received the text message: ${context.event.text}`);
 
-    // Text to speech
-    // var unirest = require('unirest');
-
-    // var req = unirest('GET', 'https://voicerss-text-to-speech.p.rapidapi.com/');
-
-    // req.query({
-    //   r: '2',
-    //   c: 'mp3',
-    //   f: '8khz_8bit_mono',
-    //   src: context.event.text,
-    //   hl: 'en-us',
-    //   key: 'fcb29959965b44a6837885911022e7ae',
-    // });
-
-    // req.headers({
-    //   'x-rapidapi-host': 'voicerss-text-to-speech.p.rapidapi.com',
-    //   'x-rapidapi-key': '54ae8d0eeemsh58d84513f7c39dbp199060jsn5fb9173b2278',
-    // });
-
-    // req.end(function (res) {
-    //   if (res.error) throw new Error(res.error);
-
-    //   console.log(res.body);
-    // });
-
-    await context.sendAudio({
-      originalContentUrl: 'https://example.com/audio.mp3',
-      duration: 240000,
-    });
-  }
-
-  if (context.event.isPayload) {
+    const quickReply = {
+      items: [
+        {
+          type: 'action',
+          action: {
+            type: 'cameraRoll',
+            label: 'Send photo',
+          },
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'camera',
+            label: 'Open camera',
+          },
+        },
+      ],
+    };
+    await context.sendText('hello', { quickReply });
+  } else if (context.event.isPayload) {
     await context.sendText(`received the payload: ${context.event.payload}`);
-  }
-
-  if (context.event.isImage) {
+  } else if (context.event.isImage) {
     await context.sendImage({
       originalContentUrl: 'https://example.com/image.jpg',
       previewImageUrl: 'https://example.com/preview.jpg',
@@ -101,23 +87,17 @@ module.exports = async function App(context) {
 
     // // You can do whatever you want, for example, write buffer into file system
     // await fs.promises.writeFile(filename, buffer);
-  }
-
-  if (context.event.isVideo) {
+  } else if (context.event.isVideo) {
     await context.sendVideo({
       originalContentUrl: 'https://example.com/video.mp4',
       previewImageUrl: 'https://example.com/preview.jpg',
     });
-  }
-
-  if (context.event.isAudio) {
+  } else if (context.event.isAudio) {
     await context.sendAudio({
       originalContentUrl: 'https://example.com/audio.mp3',
       duration: 240000,
     });
-  }
-
-  if (context.event.text == 'Hi') {
+  } else if (context.event.text == 'Hi') {
     await context.sendText('Hi');
     const imagemap = {
       baseUrl: 'https://via.placeholder.com/800',
@@ -150,36 +130,32 @@ module.exports = async function App(context) {
     };
     const altText = 'this is an imagemap';
     await context.sendImagemap(altText, imagemap);
-  }
+  } else if (context.event.text == 'Shopping') {
+    // const template = {
+    //   thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
+    //   title: 'Menu',
+    //   text: 'Please select',
+    //   actions: [
+    //     {
+    //       type: 'postback',
+    //       label: 'Buy',
+    //       data: 'action=buy&itemid=123',
+    //     },
+    //     {
+    //       type: 'postback',
+    //       label: 'Add to cart',
+    //       data: 'action=add&itemid=123',
+    //     },
+    //     {
+    //       type: 'uri',
+    //       label: 'View detail',
+    //       uri: 'http://example.com/page/123',
+    //     },
+    //   ],
+    // };
+    // const altText = 'this is a button template';
+    // await context.sendButtonTemplate(altText, template);
 
-  if (context.event.text == 'Shopping') {
-    const template = {
-      thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
-      title: 'Menu',
-      text: 'Please select',
-      actions: [
-        {
-          type: 'postback',
-          label: 'Buy',
-          data: 'action=buy&itemid=123',
-        },
-        {
-          type: 'postback',
-          label: 'Add to cart',
-          data: 'action=add&itemid=123',
-        },
-        {
-          type: 'uri',
-          label: 'View detail',
-          uri: 'http://example.com/page/123',
-        },
-      ],
-    };
-    const altText = 'this is a button template';
-    await context.sendButtonTemplate(altText, template);
-  }
-
-  if (context.event.text == 'Shopping') {
     const template = [
       {
         thumbnailImageUrl: 'https://example.com/bot/images/item1.jpg',
@@ -228,5 +204,7 @@ module.exports = async function App(context) {
     ];
     const altText = 'this is a carousel template';
     await context.sendCarouselTemplate(altText, template);
+  } else {
+    await context.sendText('Sorry, typo eh?');
   }
 };
