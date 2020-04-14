f77877fa131a4a9ba823a2f6016ef484c7f43117
novitaguok <novitaguok@gmail.com>
2020-04-14 20:17:40 +0700

    Fix

--- src/index.js
+++ src/index.js
@@ -159,6 +159,114 @@ module.exports = async function App(context) {
     await context.sendText('Hi! Wanna have fun? Tap on any buttons below', {
       quickReply,
     });
+    if (context.event.text == 'Hi') {
+      await context.sendText('Hi');
+      const imagemap = {
+        baseUrl: 'https://via.placeholder.com/800',
+        baseSize: {
+          height: 1040,
+          width: 1040,
+        },
+        actions: [
+          {
+            type: 'uri',
+            linkUri: 'https://placeholder.com/',
+            area: {
+              x: 0,
+              y: 0,
+              width: 520,
+              height: 1040,
+            },
+          },
+          {
+            type: 'message',
+            text: 'hello',
+            area: {
+              x: 520,
+              y: 0,
+              width: 520,
+              height: 1040,
+            },
+          },
+        ],
+      };
+      const altText = 'this is an imagemap';
+      await context.sendImagemap(altText, imagemap);
+    } else if (context.event.text == 'Shopping') {
+      // const template = {
+      //   thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
+      //   title: 'Menu',
+      //   text: 'Please select',
+      //   actions: [
+      //     {
+      //       type: 'postback',
+      //       label: 'Buy',
+      //       data: 'action=buy&itemid=123',
+      //     },
+      //     {
+      //       type: 'postback',
+      //       label: 'Add to cart',
+      //       data: 'action=add&itemid=123',
+      //     },
+      //     {
+      //       type: 'uri',
+      //       label: 'View detail',
+      //       uri: 'http://example.com/page/123',
+      //     },
+      //   ],
+      // };
+      // const altText = 'this is a button template';
+      // await context.sendButtonTemplate(altText, template);
+
+      const template = [
+        {
+          thumbnailImageUrl: 'https://example.com/bot/images/item1.jpg',
+          title: 'this is menu',
+          text: 'description',
+          actions: [
+            {
+              type: 'postback',
+              label: 'Buy',
+              data: 'action=buy&itemid=111',
+            },
+            {
+              type: 'postback',
+              label: 'Add to cart',
+              data: 'action=add&itemid=111',
+            },
+            {
+              type: 'uri',
+              label: 'View detail',
+              uri: 'http://example.com/page/111',
+            },
+          ],
+        },
+        {
+          thumbnailImageUrl: 'https://example.com/bot/images/item2.jpg',
+          title: 'this is menu',
+          text: 'description',
+          actions: [
+            {
+              type: 'postback',
+              label: 'Buy',
+              data: 'action=buy&itemid=222',
+            },
+            {
+              type: 'postback',
+              label: 'Add to cart',
+              data: 'action=add&itemid=222',
+            },
+            {
+              type: 'uri',
+              label: 'View detail',
+              uri: 'http://example.com/page/222',
+            },
+          ],
+        },
+      ];
+      const altText = 'this is a carousel template';
+      await context.sendCarouselTemplate(altText, template);
+    }
     // await context.sendText(`received the text message: ${context.event.text}`, {
     //   quickReply,
     // });
@@ -171,10 +279,11 @@ module.exports = async function App(context) {
     // // You can do whatever you want, for example, write buffer into file system
     // await fs.promises.writeFile(filename, buffer);
 
-    await context.sendImage({
-      originalContentUrl: 'https://example.com/image.jpg',
-      previewImageUrl: 'https://example.com/preview.jpg',
-    });
+    // await context.sendImage({
+    //   originalContentUrl: 'https://example.com/image.jpg',
+    //   previewImageUrl: 'https://example.com/preview.jpg',
+    await context.sendText('Nice pic!');
+    // });
   } else if (context.event.isVideo) {
     await context.sendVideo({
       originalContentUrl: 'https://example.com/video.mp4',
@@ -185,117 +294,6 @@ module.exports = async function App(context) {
       originalContentUrl: 'https://example.com/audio.mp3',
       duration: 240000,
     });
-  }
-
-  if (context.event.text == 'Hi') {
-    await context.sendText('Hi');
-    const imagemap = {
-      baseUrl: 'https://via.placeholder.com/800',
-      baseSize: {
-        height: 1040,
-        width: 1040,
-      },
-      actions: [
-        {
-          type: 'uri',
-          linkUri: 'https://placeholder.com/',
-          area: {
-            x: 0,
-            y: 0,
-            width: 520,
-            height: 1040,
-          },
-        },
-        {
-          type: 'message',
-          text: 'hello',
-          area: {
-            x: 520,
-            y: 0,
-            width: 520,
-            height: 1040,
-          },
-        },
-      ],
-    };
-    const altText = 'this is an imagemap';
-    await context.sendImagemap(altText, imagemap);
-  }
-
-  if (context.event.text == 'Shopping') {
-    // const template = {
-    //   thumbnailImageUrl: 'https://example.com/bot/images/image.jpg',
-    //   title: 'Menu',
-    //   text: 'Please select',
-    //   actions: [
-    //     {
-    //       type: 'postback',
-    //       label: 'Buy',
-    //       data: 'action=buy&itemid=123',
-    //     },
-    //     {
-    //       type: 'postback',
-    //       label: 'Add to cart',
-    //       data: 'action=add&itemid=123',
-    //     },
-    //     {
-    //       type: 'uri',
-    //       label: 'View detail',
-    //       uri: 'http://example.com/page/123',
-    //     },
-    //   ],
-    // };
-    // const altText = 'this is a button template';
-    // await context.sendButtonTemplate(altText, template);
-
-    const template = [
-      {
-        thumbnailImageUrl: 'https://example.com/bot/images/item1.jpg',
-        title: 'this is menu',
-        text: 'description',
-        actions: [
-          {
-            type: 'postback',
-            label: 'Buy',
-            data: 'action=buy&itemid=111',
-          },
-          {
-            type: 'postback',
-            label: 'Add to cart',
-            data: 'action=add&itemid=111',
-          },
-          {
-            type: 'uri',
-            label: 'View detail',
-            uri: 'http://example.com/page/111',
-          },
-        ],
-      },
-      {
-        thumbnailImageUrl: 'https://example.com/bot/images/item2.jpg',
-        title: 'this is menu',
-        text: 'description',
-        actions: [
-          {
-            type: 'postback',
-            label: 'Buy',
-            data: 'action=buy&itemid=222',
-          },
-          {
-            type: 'postback',
-            label: 'Add to cart',
-            data: 'action=add&itemid=222',
-          },
-          {
-            type: 'uri',
-            label: 'View detail',
-            uri: 'http://example.com/page/222',
-          },
-        ],
-      },
-    ];
-    const altText = 'this is a carousel template';
-    await context.sendCarouselTemplate(altText, template);
   } else {
     if (context.event.isText) await context.sendText('Sorry, typo eh?');
   }
